f2c8fcda1c4c523116473bbd9f19af1b841bbdc6
anfishka <venera131916@gmail.com>
2023-05-25 09:03:32 +0300

    add changes

--- structure/structure/change_db_students.cpp
+++ structure/structure/change_db_students.cpp
@@ -9,6 +9,32 @@
 #include "menu_student.h"
 using namespace std;
 
+void delete_student(vector<STUDENT>& db_students)
+{
+	if (db_students.empty()) {
+		cout << "The student database is empty." << endl;
+		return;
+	}
+
+	cout << "Enter the last name of the student you want to delete: ";
+	string lastName;
+	cin >> lastName;
+
+	// »щем студента с указанной фамилией
+	auto it = find_if(db_students.begin(), db_students.end(), [&](const STUDENT& student) {
+		return student.lastName == lastName;
+		});
+
+	if (it != db_students.end()) {
+		// ”дал€ем студента из вектора
+		db_students.erase(it);
+		cout << "Student \"" << lastName << "\" has been deleted." << endl;
+	}
+	else {
+		cout << "Student \"" << lastName << "\" not found." << endl;
+	}
+}
+
 void change_db_students(vector <STUDENT>& db_students)
 {
 	int choice;
@@ -32,13 +58,11 @@ void change_db_students(vector <STUDENT>& db_students)
 			}
 			db_students.push_back(stud);
 			print_students(db_students);
-			break;
-			
-
+			break;	
 		case 2:
-			break;
+			delete_student(db_students);
 		case 3:
-			break;
+			return;
 		}
 	} while (choice != 3);
 	menu_student();
